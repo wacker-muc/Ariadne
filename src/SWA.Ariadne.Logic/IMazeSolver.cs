efe9a66691c243fab53fb6d3b8ee7e544990669f
SWA (WACKER) <stephan@wacker-muc.de>
2007-12-31 13:08:52 +0000

    code distributed to several projects

---
+++ src/SWA.Ariadne.Logic/IMazeSolver.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWA.Ariadne.Logic
+{
+    interface IMazeSolver
+    {
+        void Step();
+    }
+}
---
+++ src/SWA.Ariadne.Logic/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SWA.Ariadne.Logic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("T-Systems GEI GmbH")]
+[assembly: AssemblyProduct("SWA.Ariadne.Logic")]
+[assembly: AssemblyCopyright("Copyright © T-Systems GEI GmbH 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("48d39e4d-90b4-49f4-b151-f4b280e158b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
