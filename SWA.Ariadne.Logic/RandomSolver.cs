75aee6152c708440d12bfde44ebd81b23cd8db9d
SWA (WACKER) <stephan@wacker-muc.de>
2008-01-01 07:10:25 +0000

    implemented RandomSolver strategy

--- SWA.Ariadne.Logic/RandomSolver.cs
+++ SWA.Ariadne.Logic/RandomSolver.cs
@@ -14,6 +14,10 @@ namespace SWA.Ariadne.Logic
         /// </summary>
         private Random random;
 
+        private Stack<MazeSquare> stack = new Stack<MazeSquare>();
+
+        private bool forward = true;
+
         #endregion
 
         #region Constructor
@@ -22,35 +26,72 @@ namespace SWA.Ariadne.Logic
             : base(maze)
         {
             this.random = new Random();
+            this.Reset();
         }
 
         #endregion
 
-        public void Step()
+        public void Reset()
         {
-            if (maze.Solved())
-            {
-                return;
-            }
+            stack.Clear();
 
-            // possible choices
-            int[] xNext = new int[4];
-            int[] yNext = new int[4];
-            int numNext = 0;
+            // Move to the start square.
+            MazeSquare sq = maze.StartSquare;
 
-            // minimum of visited count of the current square's neighbors
-            byte minVisited = 2;
-
-            // TODO: find most promising neighbor squares
-
-            // select one of the neighbor squares
-            int iNext = random.Next(numNext);
-
-            // TODO: draw a path from xyCur to xyNext[iNext]
-
-            xCur = xNext[iNext];
-            yCur = yNext[iNext];
+            // Push the start square onto the stack.
+            stack.Push(sq);
+            sq.isVisited = true;
         }
 
+        public override void Step(out MazeSquare sq1, out MazeSquare sq2, out bool forward)
+        {
+            if (maze.IsSolved)
+            {
+                throw new Exception("Maze is already solved.");
+            }
+
+            // Get the current square (at xCur, yCur)
+            sq1 = stack.Peek();
+
+            // Possible choices
+            List<MazeSquare.WallPosition> openWalls = new List<MazeSquare.WallPosition>(MazeSquare.WP_NUM);
+
+            // Collect the unfixed walls of sq1.
+            //
+            for (MazeSquare.WallPosition wp = MazeSquare.WP_MIN; wp <= MazeSquare.WP_MAX; wp++)
+            {
+                if (sq1[wp] == MazeSquare.WallState.WS_OPEN)
+                {
+                    MazeSquare sq = sq1.NeighborSquare(wp);
+
+                    if (!sq.isVisited)
+                    {
+                        openWalls.Add(wp);
+                    }
+                } // if WS_MAYBE
+            } // foreach wp
+
+            if (openWalls.Count > 0)
+            {
+                // select one of the neighbor squares
+                MazeSquare.WallPosition wp = openWalls[random.Next(openWalls.Count)];
+
+                sq2 = sq1.NeighborSquare(wp);
+                forward = true;
+
+                // Push the next square onto the stack.
+                stack.Push(sq2);
+                sq2.isVisited = true;
+
+            }
+            else
+            {
+                // Pop the current square from the stack.
+                stack.Pop();
+
+                sq2 = stack.Peek();
+                forward = false;
+            }
+        }
     }
 }
