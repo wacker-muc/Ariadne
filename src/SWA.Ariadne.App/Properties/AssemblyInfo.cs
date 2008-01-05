757fd39dae56ebea3c183297bc64fc5c99539afe
SWA (WACKER) <stephan@wacker-muc.de>
2008-01-05 14:15:35 +0000

    updated AssemblyVersion number

--- src/SWA.Ariadne.App/Properties/AssemblyInfo.cs
+++ src/SWA.Ariadne.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.15")]
-[assembly: AssemblyFileVersion("1.0.0.15")]
+[assembly: AssemblyVersion("1.1.0.40")]
+[assembly: AssemblyFileVersion("1.1.0.40")]
