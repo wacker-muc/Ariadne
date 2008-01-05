a71c743d28f621693c93530aa95c08838b3f65f1
SWA (WACKER) <stephan@wacker-muc.de>
2008-01-05 15:36:55 +0000

    evaluate all Auto... check box settings

--- SWA.Ariadne.App/Properties/AssemblyInfo.cs
+++ SWA.Ariadne.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.40")]
-[assembly: AssemblyFileVersion("1.1.0.40")]
+[assembly: AssemblyVersion("1.1.0.41")]
+[assembly: AssemblyFileVersion("1.1.0.41")]
