f29ef1c473522c76e90b7b10fe986ee5c9b3acda
SWA (WACKER) <stephan@wacker-muc.de>
2008-01-06 11:01:36 +0000

    delete the explicit datasource - not required and caused errors in the designer

--- src/SWA.Ariadne.App/Properties/AssemblyInfo.cs
+++ src/SWA.Ariadne.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.49")]
-[assembly: AssemblyFileVersion("1.1.0.49")]
+[assembly: AssemblyVersion("1.1.0.50")]
+[assembly: AssemblyFileVersion("1.1.0.50")]
