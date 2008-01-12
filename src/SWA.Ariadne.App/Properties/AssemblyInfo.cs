fd10c41d909cb3e894b850e5e547958f7eb906d5
SWA (WACKER) <stephan@wacker-muc.de>
2008-01-12 15:12:42 +0000

    made AriadneFormBase a non-abstract class

--- src/SWA.Ariadne.App/Properties/AssemblyInfo.cs
+++ src/SWA.Ariadne.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.0.65")]
-[assembly: AssemblyFileVersion("1.2.0.65")]
+[assembly: AssemblyVersion("1.2.0.66")]
+[assembly: AssemblyFileVersion("1.2.0.66")]
