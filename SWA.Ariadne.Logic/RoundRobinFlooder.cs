a2ad89ccc76e06a04ad9830f6be3801f2fde8d15
SWA (WACKER) <stephan@wacker-muc.de>
2008-02-03 09:31:08 +0000

    refactored common logic of Flooder classes into the Flooder.IStep() method

--- SWA.Ariadne.Logic/RoundRobinFlooder.cs
+++ SWA.Ariadne.Logic/RoundRobinFlooder.cs
@@ -11,15 +11,6 @@ namespace SWA.Ariadne.Logic
     /// </summary>
     internal class RoundRobinFlooder : Flooder
     {
-        #region Member variables
-
-        /// <summary>
-        /// All squares passed in forward direction are collected in a queue.
-        /// </summary>
-        private Queue<MazeSquare> queue = new Queue<MazeSquare>();
-
-        #endregion
-
         #region Constructor
 
         /// <summary>
@@ -34,87 +25,26 @@ namespace SWA.Ariadne.Logic
 
         #endregion
 
-        #region Setup methods
-
-        /// <summary>
-        /// Reset to the initial state (before the maze is solved).
-        /// Subclasses should call their base class' method first.
-        /// </summary>
-        public override void Reset()
-        {
-            base.Reset();
-
-            queue.Clear();
-
-            // Move to the start square.
-            MazeSquare sq = maze.StartSquare;
-
-            // Add the start square to the queue.
-            queue.Enqueue(sq);
-            sq.isVisited = true;
-        }
-
-        #endregion
-
         #region Runtime methods
 
         /// <summary>
-        /// Travel from one visited square to a neighbor square (through an open wall).
+        /// Select an index within the flooder's list of open paths.
         /// </summary>
-        /// <param name="sq1">first (previously visited) square</param>
-        /// <param name="sq2">next (neighbor) square</param>
-        /// <param name="forward">true if the neighbor square was not visited previously</param>
-        protected override void StepI(out MazeSquare sq1, out MazeSquare sq2, out bool forward)
+        /// <returns></returns>
+        protected override int SelectPathIdx()
         {
-            if (maze.IsSolved)
-            {
-                throw new Exception("Maze is already solved.");
-            }
-
-            List<MazeSquare.WallPosition> openWalls;
-            
-            while(true)
-            {
-                // Get the current square but leave it in the queue.
-                sq1 = queue.Peek();
-
-                // Possible choices of open walls (not visited).
-                openWalls = SolverBase.OpenWalls(sq1, true);
-
-                if (openWalls.Count == 0)
-                {
-                    queue.Dequeue();
-                    MarkDeadBranch(sq1);
-                }
-                else
-                {
-                    // If this was the last open wall of sq1, it can be removed from the queue.
-                    if (openWalls.Count == 1)
-                    {
-                        queue.Dequeue();
-                    }
-
-                    break;
-                }
-            }
-
-            // Select one (any) of the neighbor squares.
-            MazeSquare.WallPosition wp = openWalls[0];
-
-            sq2 = sq1.NeighborSquare(wp);
-            forward = true;
-
-            // Add the next square to the queue.
-            queue.Enqueue(sq2);
-            sq2.isVisited = true;
+            return 0;
         }
 
         /// <summary>
-        /// Current number of open paths.
+        /// Select one of the open walls leading away from the given square.
         /// </summary>
-        protected override int CountOpenPaths
+        /// <param name="sq1"></param>
+        /// <param name="openWalls"></param>
+        /// <returns></returns>
+        protected override MazeSquare.WallPosition SelectDirection(MazeSquare sq1, List<MazeSquare.WallPosition> openWalls)
         {
-            get { return this.queue.Count; }
+            return openWalls[0];
         }
 
         #endregion
