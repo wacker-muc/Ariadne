ac865fc90e94e4ab4c56195e26a3d5f9d65681b7
SWA (WACKER) <stephan@wacker-muc.de>
2008-03-31 21:30:24 +0000

    make the screen saver the active application (capturing keystrokes)

--- src/SWA.Ariadne.App/ScreenSaverForm.Designer.cs
+++ src/SWA.Ariadne.App/ScreenSaverForm.Designer.cs
@@ -93,7 +93,6 @@ namespace SWA.Ariadne.App
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(643, 300);
             this.Controls.Add(this.outerInfoPanel);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ScreenSaverForm";
             this.Text = "ScreenSaverForm";
             this.Load += new System.EventHandler(this.ScreenSaverForm_Load);
