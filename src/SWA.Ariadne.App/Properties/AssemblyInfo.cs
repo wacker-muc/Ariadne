acfb8ea1f9f7c23125b0102b1eca06b04a3b5d7d
SWA (WACKER) <stephan@wacker-muc.de>
2008-04-05 12:39:02 +0000

    improved performance of DeadEndChecker

--- src/SWA.Ariadne.App/Properties/AssemblyInfo.cs
+++ src/SWA.Ariadne.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0.105")]
-[assembly: AssemblyFileVersion("2.0.0.105")]
+[assembly: AssemblyVersion("2.1.0.106")]
+[assembly: AssemblyFileVersion("2.1.0.106")]
