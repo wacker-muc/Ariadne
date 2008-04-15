d23d9a43cc6045348a7892db17f543838fa6ce47
SWA (WACKER) <stephan@wacker-muc.de>
2008-04-15 20:15:24 +0000

    renamed BorderFlooder to CornerFlooder

--- SWA.Ariadne.Logic/CornerFlooder.cs
+++ SWA.Ariadne.Logic/CornerFlooder.cs
@@ -9,7 +9,7 @@ namespace SWA.Ariadne.Logic
     /// A MazeSolver with many concurrent paths.
     /// Prefers visiting the square farthest away from the center of the maze.
     /// </summary>
-    class BorderFlooder : CenterFlooder
+    class CornerFlooder : CenterFlooder
     {
         #region Constructor
 
@@ -18,7 +18,7 @@ namespace SWA.Ariadne.Logic
         /// </summary>
         /// <param name="maze"></param>
         /// <param name="mazeDrawer"></param>
-        public BorderFlooder(Maze maze, IMazeDrawer mazeDrawer)
+        public CornerFlooder(Maze maze, IMazeDrawer mazeDrawer)
             : base(maze, mazeDrawer)
         {
             // Invert the parent strategy: Maximize distance from the reference square.
