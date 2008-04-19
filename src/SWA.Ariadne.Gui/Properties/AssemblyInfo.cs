5e90ecf3b41897c6f72e206143a2f5094dc17421
SWA (WACKER) <stephan@wacker-muc.de>
2008-04-19 12:49:20 +0000

    created new project SWA.Ariadne.Gui; moved most of the files from SWA.Ariadne.App there

---
+++ src/SWA.Ariadne.Gui/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SWA.Ariadne.App")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Stephan Wacker")]
+[assembly: AssemblyProduct("SWA.Ariadne.Gui")]
+[assembly: AssemblyCopyright("Copyright © Stephan Wacker 2008")]
+[assembly: AssemblyTrademark("SWA")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c3a4644f-43ae-4fcd-9a30-2bede308eed7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.1.0.122")]
+[assembly: AssemblyFileVersion("2.1.0.122")]
