801f8d95c87e0009cf3ca815291bd4768e964a8f
SWA (WACKER) <stephan@wacker-muc.de>
2008-05-11 09:16:24 +0000

    when selecting images to be painted in the maze: avoid recently used images

--- SWA.Ariadne.App/Properties/AssemblyInfo.cs
+++ SWA.Ariadne.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.0.144")]
-[assembly: AssemblyFileVersion("2.2.0.144")]
+[assembly: AssemblyVersion("2.2.0.145")]
+[assembly: AssemblyFileVersion("2.2.0.145")]
