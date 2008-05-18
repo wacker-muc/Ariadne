693e00b720d3bbf65b8738c680b938cab5f78f13
SWA (WACKER) <stephan@wacker-muc.de>
2008-05-18 08:41:13 +0000

    added icons to the remaining menu buttons

--- SWA.Ariadne.Gui/Properties/AssemblyInfo.cs
+++ SWA.Ariadne.Gui/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0.154")]
-[assembly: AssemblyFileVersion("2.3.0.154")]
+[assembly: AssemblyVersion("2.3.0.155")]
+[assembly: AssemblyFileVersion("2.3.0.155")]
