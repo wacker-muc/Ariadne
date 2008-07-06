a667319c877a6300513f39cff68d5affdffc43d5
SWA (swacker) <stephan@wacker-muc.de>
2008-07-06 16:55:15 +0000

    add Version 1 Maze encoding schema: 6 characters (Version 0: 12 characters)

--- SWA.Ariadne.Model/Properties/AssemblyInfo.cs
+++ SWA.Ariadne.Model/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.0.153")]
-[assembly: AssemblyFileVersion("2.2.0.153")]
+[assembly: AssemblyVersion("2.3.0.183")]
+[assembly: AssemblyFileVersion("2.3.0.183")]
