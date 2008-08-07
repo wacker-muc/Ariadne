d4a2c29043bb2770c13bf4fd3bc429932ff840cd
SWA (swacker) <stephan@wacker-muc.de>
2008-08-07 18:20:28 +0000

    new TilesOutlineShape: Pentominoes

--- src/SWA.Ariadne.App/Properties/AssemblyInfo.cs
+++ src/SWA.Ariadne.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.0.233")]
-[assembly: AssemblyFileVersion("3.1.0.233")]
+[assembly: AssemblyVersion("3.1.0.234")]
+[assembly: AssemblyFileVersion("3.1.0.234")]
