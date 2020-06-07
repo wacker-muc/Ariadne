f2554a9568992cb6a3c3c249dd75f416fc22d319
SWA <git@wacker-muc.de>
2020-06-07 11:43:06 -0400

    Fully functional on Linux
    * assembly version 4.0.1.342

--- src.CS/SWA.Ariadne.App/Properties/AssemblyInfo.cs
+++ src.CS/SWA.Ariadne.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.0.1.340")]
-[assembly: AssemblyFileVersion("4.0.1.340")]
+[assembly: AssemblyVersion("4.0.1.342")]
+[assembly: AssemblyFileVersion("4.0.1.342")]
